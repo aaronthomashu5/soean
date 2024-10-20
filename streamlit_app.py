28f68df8bf3775dc59d4c369e65271fd0ae38b8a
Aaron <124760049+aaronthomashu5@users.noreply.github.com>
2024-10-20 20:53:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,16 @@
 import streamlit as st
 import requests
 
-# Streamlit app
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+# Your app code goes here
 def main():
     st.title("IBM AI-Powered Wireless Communication Assistant")
     st.write("Enter your query related to Wireless Communication:")
