5332505430c704b58b38201312e898d6ca0d8816
Aaron <124760049+aaronthomashu5@users.noreply.github.com>
2024-10-20 15:09:02 +0000

    hide github

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,18 @@ import streamlit as st
 import requests
 
 # Streamlit app
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 def main():
     st.title("IBM AI-Powered Wireless Communication Assistant")
     st.write("Enter your query related to Wireless Communication:")
